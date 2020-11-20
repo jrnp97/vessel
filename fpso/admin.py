dbb4b7a13480a0b511de3ecbf893652b1d8c4717
jrnp97 <jrnp1997@gmail.com>
2020-11-20 14:33:42 -0300

    :pencil: Defining data structure

--- fpso/admin.py
+++ fpso/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from fpso.models import Vessel
+from fpso.models import Equipment
+
+
+@admin.register(Vessel)
+class VesselAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Equipment)
+class EquipementAdmin(admin.ModelAdmin):
+    pass
+
--- fpso/models.py
+++ fpso/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vessel(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.code}'
+
+class Equipment(models.Model):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    vessel = models.ForeignKey(Vessel, on_delete=models.PROTECT)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+   
+    class Meta:
+        unique_together = ('vessel', 'code')
+
+    def __str__(self):
+        return f'{self.vessel}: {self.code} - {self.name}'
+
+
